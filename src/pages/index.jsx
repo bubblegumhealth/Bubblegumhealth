b1264f46a2b63e7e623b2f3c4b7f9c911d93b50e
ebuberoderick <ebuberoderick2@gmail.com>
2025-06-25 16:56:15 +0100

    updating home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import BlogChip from "@/components/organisms/BlogChip";
 import OurDoctors from "@/components/organisms/OurDoctors";
 import ReportRapeForm from "@/components/organisms/ReportRapeForm";
 import { DM_Serif_Display } from "next/font/google";
+import Link from "next/link";
 
 
 
@@ -18,6 +19,40 @@ export default function Home() {
   return (
     <AppLayout>
       <div className="bg-bub-secondary">
+        <div className="h-screen p-12">
+          <div className="homeBannerImg h-full rounded-2xl overflow-hidden">
+            <div className="h-full bg-gradient-to-b from-transparent from-55% to-black px-8 py-20 flex items-end">
+              <div className="w-full space-y-8">
+                <div className="max-w-xl text-white space-y-3 p-4">
+                  <div className="text-6xl font-bold">
+                    <div className="">Breaking <span className={`${dmSerifDisplay.className} italic`}>Stigmas</span></div>
+                    <div className="">Bursting <span className={`${dmSerifDisplay.className} italic`}>Flavors</span></div>
+                  </div>
+                  <div className="text-xl font-medium">Premium brands, tested reliability, from our door to yours, no questions asked.</div>
+                </div>
+                <div className="flex gap-4 items-center">
+                  <Link href='/shop'>
+                    <div className="font-semibold text-white bg-bub-primary rounded-full px-6 py-4">Shop Now</div>
+                  </Link>
+                  <Link href='/talk_to_a_doctor'>
+                    <div className="border border-white text-white font-semibold rounded-full px-6 py-4">Speak to a Specialist</div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-10">
+          <div className={`${dmSerifDisplay.className} text-[#E0BBB1] text-8xl text-center max-w-5xl mx-auto`}>choose your adventure <span className="italic">responsibly</span></div>
+          <div className="flex justify-center">
+            <Link href='/shop'>
+              <div className="font-semibold text-white bg-bub-primary rounded-full px-6 py-4">Shop Now</div>
+            </Link>
+          </div>
+        </div>
+
+
         <div className="flex max-w-5xl mx-auto gap-[72px] py-24">
           <div className="">
             <div className="sideImg2 h-[475.86px] w-[300px] rounded-2xl" />
