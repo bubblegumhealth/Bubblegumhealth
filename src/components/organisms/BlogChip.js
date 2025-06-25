23bf759cfd233299cffd0364b745a61561930c80
ebuberoderick <ebuberoderick2@gmail.com>
2025-06-25 17:08:56 +0100

    fixed deploymentbug

--- src/components/organisms/BlogChip.js
+++ src/components/organisms/BlogChip.js
@@ -5,8 +5,8 @@ function BlogChip() {
         <div className='space-y-5'>
             <div className='h-[300px] rounded-2xl bg-gray-50'></div>
             <div className='space-y-3'>
-                <div className='font-semibold text-2xl'>What Actually Happens During Testing (Spoiler: It's Really Not That Bad)</div>
-                <div className='text-gray-400'>Most people's anxiety about STI testing comes from not knowing what to expect. Here's the honest breakdown of what actually happens:</div>
+                <div className='font-semibold text-2xl'>What Actually Happens During Testing (Spoiler: It&apos;s Really Not That Bad)</div>
+                <div className='text-gray-400'>Most people&apos;s anxiety about STI testing comes from not knowing what to expect. Here&apos;s the honest breakdown of what actually happens:</div>
             </div>
         </div>
     )
